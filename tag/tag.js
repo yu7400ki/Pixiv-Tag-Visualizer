7111046d17e2d9295a88025840bc3f758d2a8c8a
yu7400ki <yu7400ki@gmail.com>
2022-10-20 02:11:35 +0900

    インラインsvg化

--- tag/tag.js
+++ tag/tag.js
@@ -1,3 +1,37 @@
+const checkSvg = `<svg
+width="24"
+height="24"
+viewBox="0 0 24 24"
+fill="none"
+xmlns="http://www.w3.org/2000/svg"
+>
+<path
+  d="M10.2426 16.3137L6 12.071L7.41421 10.6568L10.2426 13.4853L15.8995 7.8284L17.3137 9.24262L10.2426 16.3137Z"
+  fill="currentColor"
+/>
+<path
+  fill-rule="evenodd"
+  clip-rule="evenodd"
+  d="M1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21Z"
+  fill="currentColor"
+/>
+</svg>`
+
+const lockSvg = `<svg
+width="24"
+height="24"
+viewBox="0 0 24 24"
+fill="none"
+xmlns="http://www.w3.org/2000/svg"
+>
+<path
+  fill-rule="evenodd"
+  clip-rule="evenodd"
+  d="M18 10.5C19.6569 10.5 21 11.8431 21 13.5V19.5C21 21.1569 19.6569 22.5 18 22.5H6C4.34315 22.5 3 21.1569 3 19.5V13.5C3 11.8431 4.34315 10.5 6 10.5V7.5C6 4.18629 8.68629 1.5 12 1.5C15.3137 1.5 18 4.18629 18 7.5V10.5ZM12 3.5C14.2091 3.5 16 5.29086 16 7.5V10.5H8V7.5C8 5.29086 9.79086 3.5 12 3.5ZM18 12.5H6C5.44772 12.5 5 12.9477 5 13.5V19.5C5 20.0523 5.44772 20.5 6 20.5H18C18.5523 20.5 19 20.0523 19 19.5V13.5C19 12.9477 18.5523 12.5 18 12.5Z"
+  fill="currentColor"
+/>
+</svg>`
+
 const getIllustId = (href) => {
   const illustId = href.match(/https:\/\/www.pixiv.net\/artworks\/(\d+)/);
   return illustId[1];
@@ -60,18 +94,14 @@ const addBadge = async (href) => {
     const tagName = link.textContent;
     if (tags[tagName]?.userId === authorId) {
       const wrapper = document.createElement("div");
-      const checkIcon = document.createElement("img");
-      checkIcon.src = "https://css.gg/check-o.svg";
       wrapper.classList.add("gg-icon-check");
-      wrapper.appendChild(checkIcon);
+      wrapper.innerHTML = checkSvg;
       el.appendChild(wrapper);
     }
     if (tags[tagName]?.locked) {
       const wrapper = document.createElement("div");
-      const lockIcon = document.createElement("img");
-      lockIcon.src = "https://css.gg/lock.svg";
       wrapper.classList.add("gg-icon-lock");
-      wrapper.appendChild(lockIcon);
+      wrapper.innerHTML = lockSvg;
       el.appendChild(wrapper);
     }
   });
