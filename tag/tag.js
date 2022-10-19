efd6fd1d45f8fc5eef947ec299b3206b5e1a181e
yu7400ki <yu7400ki@gmail.com>
2022-10-20 00:54:12 +0900

    SPAに対応するよう修正

--- tag/tag.js
+++ tag/tag.js
@@ -1,66 +1,100 @@
-const href = window.location.href;
-
-let html;
-fetch(href)
-  .then((response) => {
-    if (response.ok) {
-      return response.text();
-    }
-    throw new Error("This page is not available");
-  })
-  .then((text) => {
-    html = text;
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
-const getTags = (doc) => {
-  const preloadData = JSON.parse(
-    doc.getElementById("meta-preload-data").content
-  )
-  const _tags = preloadData.illust[getIllustId(href)].tags.tags;
-  let tags = {};
-  _tags.forEach((tag) => {
-    tags[tag.tag] = tag;
-  });
-  return tags;
-}
-
 const getIllustId = (href) => {
   const illustId = href.match(/https:\/\/www.pixiv.net\/artworks\/(\d+)/);
   return illustId[1];
 };
 
-const getAuthorId = (doc) => {
+const getTags = (doc, id) => {
   const preloadData = JSON.parse(
     doc.getElementById("meta-preload-data").content
-  )
-  return preloadData.illust[getIllustId(href)].tags.authorId;
+  );
+  const _tags = preloadData.illust[id].tags.tags;
+  let tags = {};
+  _tags.forEach((tag) => {
+    tags[tag.tag] = tag;
+  });
+  return tags;
 };
 
-window.addEventListener("load", () => {
+const getAuthorId = (doc, id) => {
+  const preloadData = JSON.parse(
+    doc.getElementById("meta-preload-data").content
+  );
+  return preloadData.illust[id].tags.authorId;
+};
+
+const getTagDom = () => {
+  return document.querySelector("ul.sc-pj1a4x-0.gZfuPH");
+};
+
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const addBadge = async (href) => {
+  if (!href.match(/https:\/\/www.pixiv.net\/artworks\/\d+/)) return;
+  const response = await fetch(href);
+  const html = await response.text();
+
   const domParser = new DOMParser();
   const doc = domParser.parseFromString(html, "text/html");
 
-  const authorId = getAuthorId(doc);
-  const tags = getTags(doc);
+  const illustId = getIllustId(href);
+  const authorId = getAuthorId(doc, illustId);
+  const tags = getTags(doc, illustId);
+  console.log(tags);
 
-  const tagDom = document.getElementsByClassName("gtm-new-work-tag-event-click");
+  await sleep(500);
+  while (getTagDom() === null) {
+    await sleep(100);
+  }
 
-  for (let i = 0; i < tagDom.length; i++) {
-    const tagName = tagDom[i].innerText;
+  const badged = document.querySelectorAll('[class^="gg-icon"]');
+  badged.forEach((el) => el.remove());
+
+  const tagDom = getTagDom().querySelectorAll("li");
+
+  tagDom.forEach((el) => {
+    el.classList.add("tag-li");
+    const link = el.querySelector("a");
+    if (!link) return;
+    const tagName = link.textContent;
     if (tags[tagName]?.userId === authorId) {
+      const wrapper = document.createElement("div");
       const checkIcon = document.createElement("img");
       checkIcon.src = "https://css.gg/check-o.svg";
-      checkIcon.classList.add("gg-icon");
-      tagDom[i].parentElement.appendChild(checkIcon);
+      wrapper.classList.add("gg-icon-check");
+      wrapper.appendChild(checkIcon);
+      el.appendChild(wrapper);
     }
-    if (tags[tagName].locked) {
+    if (tags[tagName]?.locked) {
+      const wrapper = document.createElement("div");
       const lockIcon = document.createElement("img");
       lockIcon.src = "https://css.gg/lock.svg";
-      lockIcon.classList.add("gg-icon");
-      tagDom[i].parentElement.appendChild(lockIcon);
+      wrapper.classList.add("gg-icon-lock");
+      wrapper.appendChild(lockIcon);
+      el.appendChild(wrapper);
     }
-  }
+  });
+};
+
+window.addEventListener("load", async () => {
+  let href = window.location.href;
+  const body = document.body;
+
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach(async (mutation) => {
+      if (href !== window.location.href) {
+        href = window.location.href;
+        await addBadge(href);
+      }
+    });
+  });
+
+  const config = {
+    childList: true,
+    subtree: true,
+  };
+
+  observer.observe(body, config);
+  await addBadge(href);
 });
