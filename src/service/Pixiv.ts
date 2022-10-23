af2e9fa6fa0d3105c4739c33dedaa6efd6239e2d
yu7400ki <yu7400ki@gmail.com>
2022-10-23 18:09:49 +0900

    小説用クラスの作成

--- src/service/Pixiv.ts
+++ src/service/Pixiv.ts
@@ -1,4 +1,4 @@
-import { IllustResponse, Tags } from '../types/pixiv';
+import { IllustResponse, NovelResponse, Tags } from '../types/pixiv';
 
 export class Illust {
   illust: IllustResponse;
@@ -21,6 +21,33 @@ export class Illust {
 
   tags() {
     const _tags = this.illust.body.tags.tags;
+    const tags: Tags = {};
+    _tags.forEach((tag) => {
+      tags[tag.tag] = tag;
+    });
+    return tags;
+  }
+}
+
+export class Novel {
+  novel: NovelResponse;
+
+  constructor(novel: NovelResponse) {
+    this.novel = novel;
+  }
+
+  static async init(novelId: number) {
+    const response = await fetch(`https://www.pixiv.net/ajax/novel/${novelId}`);
+    const novel: NovelResponse = await response.json();
+    return new Novel(novel);
+  }
+
+  get author() {
+    return this.novel.body.userId;
+  }
+
+  tags() {
+    const _tags = this.novel.body.tags.tags;
     const tags: Tags = {};
     _tags.forEach((tag) => {
       tags[tag.tag] = tag;
